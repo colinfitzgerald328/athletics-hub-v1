5b9fc192a25304ac2d8d70cd141a8c6688c9f002
Colin <colinfitzgerald328@gmail.com>
2023-12-09 08:25:59 -0800

    slightly longer debounce

--- app/components/2_leftSide/index.js
+++ app/components/2_leftSide/index.js
@@ -74,7 +74,7 @@ export default function LeftSide(props) {
       }
       setLoadingSearchResults(true);
       handleSearchTermChange(searchTerm);
-    }, 300);
+    }, 500);
 
     return () => clearTimeout(delayDebounceFn);
   }, [searchTerm]);
