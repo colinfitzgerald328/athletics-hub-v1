51c46d8a2ab281357beefa0c27491b4a3e73ce6c
Colin <colinfitzgerald328@gmail.com>
2023-12-09 20:11:44 -0800

    add key

--- app/components/4_RightSide/2_Recommended/index.js
+++ app/components/4_RightSide/2_Recommended/index.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 export default function Recommended(props) {
-  const similarAthletesMap = props.similar_athletes.map((similar_athlete) => (
+  const similarAthletesMap = props.similar_athletes.map((similar_athlete, index) => (
     <div
       className={styles.similarAthlete}
       onClick={() => props.setAthlete(similar_athlete)}
+      key={index}
     >
       <img src={similar_athlete.hq_image_url} className={styles.athleteImage} />
       <div className={styles.itemsHolder}>
