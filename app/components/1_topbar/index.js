6b1a74fcf1fa95585184f260dd10437b73584447
Colin <colinfitzgerald328@gmail.com>
2023-12-11 14:25:59 -0800

    add latest

--- app/components/1_topbar/index.js
+++ app/components/1_topbar/index.js
@@ -2,5 +2,14 @@ import React from "react";
 import styles from "./styles.module.css";
 
 export default function TopBar() {
-  return <div className={styles.topBar}>athletics hub</div>;
+  return (
+    <div className={styles.topBarHolder}>
+      <div className={styles.pageLabel}>
+        athletics hub
+      </div>
+      <div className={styles.pageAction}>
+        A research tool built for track and field fans
+      </div>
+    </div>
+  )
 }
