42193ac5ec9cb56ddb24f199f47748dea40b4500
Colin <colinfitzgerald328@gmail.com>
2023-12-13 18:43:49 -0800

    beta view of collections

--- app/components/5_Collections/index.js
+++ app/components/5_Collections/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateCollectionModal from "./1_Create_Collection_Modal";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import styles from "./styles.module.css";
 import TimeAgo from "javascript-time-ago";
 
@@ -13,6 +15,39 @@ TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo("en-US");
 
 export default function Collections(props) {
+  const [collection, setCollection] = useState(props.user_collections[0].detailed_athletes);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  function updateSummaryStyle(athlete) {
+    var elements = document.getElementsByClassName(styles.competitorSummary);
+    var newScrollHeight;
+
+    for (let item of elements) {
+      if (item.textContent == athlete.summary) {
+        newScrollHeight = item.scrollHeight + "px";
+      }
+    }
+
+    for (let i = 0; i < collection.length; i++) {
+      if (collection[i].aaAthleteId == athlete.aaAthleteId) {
+        if (collection[i].height == undefined) {
+          collection[i].height = newScrollHeight;
+        } else if (collection[i].height != "0px") {
+          collection[i].height = "0px";
+        } else {
+          collection[i].height = newScrollHeight;
+        }
+      }
+    }
+
+    setCollection([...collection]);
+  }
+
+  function handleSelect(index, collection) {
+    setCollection(collection)
+    setCurrentIndex(index)
+  }
+
   return (
     <div className={styles.basic}>
       <div className={styles.topItems}>
@@ -29,7 +64,10 @@ export default function Collections(props) {
         <div className={styles.collections}>
           {props.user_collections &&
             props.user_collections.map((collection, index) => (
-              <div key={index} className={styles.collection}>
+              <div
+              onClick={()=> handleSelect(index, collection.detailed_athletes)}
+              key={index}
+              className={currentIndex == index ? styles.selected : styles.collection}>
                 <div className={styles.collectionName}>
                   {collection.collection_name}
                 </div>
@@ -39,7 +77,54 @@ export default function Collections(props) {
               </div>
             ))}
         </div>
-        <div className={styles.panel}></div>
+        <div className={styles.panel}>
+          {
+            collection.map((athlete, index) => (
+              <div key={index} className={styles.competitor}>
+                <div className={styles.topItems}>
+                    <img
+                      className={styles.competitorImage}
+                      src={athlete.hq_image_url}
+                    />
+                  <div className={styles.criticalInfo}>
+                    <div className={styles.leftItems}>
+                        <div
+                          // onClick={() =>
+                          //   props.setAthleteFromTopCompetitors(
+                          //     competitor.aaAthleteId,
+                          //   )
+                          // }
+                          className={styles.competitorName}
+                        >
+                          {athlete.full_name}
+                        </div>
+                        <div className={styles.disciplines}>
+                          {athlete.disciplines}
+                        </div>
+                    </div>
+                    <div
+                      onClick={() => updateSummaryStyle(athlete)}
+                      className={styles.rightItems}
+                    >
+                      {athlete.summary &&
+                        (athlete.height && athlete.height != "0px" ? (
+                          <RemoveIcon sx={{ fontWeight: "bold" }} />
+                        ) : (
+                          <AddIcon />
+                        ))}
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className={styles.competitorSummary}
+                  style={{ height: athlete.height }}
+                >
+                  {athlete.summary}
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
