331c7086b0b5219024148a62ee47dfb279d22a12
Colin <colinfitzgerald328@gmail.com>
2023-12-18 18:16:46 -0800

    prettier

--- app/components/3_Middle/index.js
+++ app/components/3_Middle/index.js
@@ -4,42 +4,68 @@ import styles from "./styles.module.css";
 import { Skeleton } from "@mui/material";
 
 export default function AthleteBreakDown(props) {
-  const disciplinesArr = props.athlete.length !== 0 ? props.athlete.disciplines.split(", ") : undefined
+  const disciplinesArr =
+    props.athlete.length !== 0
+      ? props.athlete.disciplines.split(", ")
+      : undefined;
   return (
     <div className={styles.athleteBreakdown}>
-        <div className={styles.itemsContainer}>
-        {
-          props.loadingNewAthlete ? 
-          <Skeleton animation="wave" height={230} variant="rectangular"></Skeleton>
-          :
-          <img src={props.athlete.hq_image_url} className={styles.athleteImage} />
-        }
+      <div className={styles.itemsContainer}>
+        {props.loadingNewAthlete ? (
+          <Skeleton
+            animation="wave"
+            height={230}
+            variant="rectangular"
+          ></Skeleton>
+        ) : (
+          <img
+            src={props.athlete.hq_image_url}
+            className={styles.athleteImage}
+          />
+        )}
         <div className={styles.athleteNameHolder}>
-          {
-            props.loadingNewAthlete ? 
-            <Skeleton sx={{"zIndex": 1000}} animation="wave" variant="circular" width={150} height={150}></Skeleton>
-            :
-            <img src={props.athlete.hq_image_url} className={styles.profileImage}/>
-          }
-        <div className={styles.nameVariables}>
-          {
-            props.loadingNewAthlete ? 
-            <Skeleton animation="wave" width={200} height={35} variant="rectangular"></Skeleton>
-            :
-            <div className={styles.fullName}>{props.athlete.full_name}</div>
-          }
-                    {
-            props.loadingNewAthlete ? 
-            <Skeleton sx={{"marginTop": "5px"}} animation="wave" width={300} height={35}></Skeleton>
-            :
-                        <div className={styles.disciplines}>
-            {disciplinesArr && disciplinesArr.map((discipline, index) => 
-              <div key={index} className={styles.tag}>
-                {discipline}
+          {props.loadingNewAthlete ? (
+            <Skeleton
+              sx={{ zIndex: 1000 }}
+              animation="wave"
+              variant="circular"
+              width={150}
+              height={150}
+            ></Skeleton>
+          ) : (
+            <img
+              src={props.athlete.hq_image_url}
+              className={styles.profileImage}
+            />
+          )}
+          <div className={styles.nameVariables}>
+            {props.loadingNewAthlete ? (
+              <Skeleton
+                animation="wave"
+                width={200}
+                height={35}
+                variant="rectangular"
+              ></Skeleton>
+            ) : (
+              <div className={styles.fullName}>{props.athlete.full_name}</div>
+            )}
+            {props.loadingNewAthlete ? (
+              <Skeleton
+                sx={{ marginTop: "5px" }}
+                animation="wave"
+                width={300}
+                height={35}
+              ></Skeleton>
+            ) : (
+              <div className={styles.disciplines}>
+                {disciplinesArr &&
+                  disciplinesArr.map((discipline, index) => (
+                    <div key={index} className={styles.tag}>
+                      {discipline}
+                    </div>
+                  ))}
               </div>
             )}
-          </div>
-          }
           </div>
         </div>
       </div>
