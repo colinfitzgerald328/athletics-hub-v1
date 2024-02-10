2844ea65312d968986652c9b36b7c463f3b476f8
Colin <colinfitzgerald328@gmail.com>
2024-02-10 12:50:02 -0800

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: "Athletics Hub",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   );
