ac5f3a9ccc56b4fc19e0961aaa9f569c1495868a
Colin <colinfitzgerald328@gmail.com>
2024-02-24 19:06:27 -0800

    Update the UI to improve user experience and fix minor bugs.

--- shortcuts.sh
+++ shortcuts.sh
@@ -1,5 +1,1 @@
 alias push_with_commit_message='git add . && git commit -m "$(git diff | curl -X POST -d @- https://athletics-hub.uc.r.appspot.com/api/generate-commit-message -H "Content-Type: text/plain")" && git push origin main'
-
-
-
-hello world how are yo udoing 
