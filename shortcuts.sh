ebdbb704f5243386afb2af54452838f1d51465e0
Colin <colinfitzgerald328@gmail.com>
2024-02-24 19:05:44 -0800

    Update shortcuts.sh to use the correct API URL for generating commit messages

--- shortcuts.sh
+++ shortcuts.sh
@@ -1,1 +1,5 @@
-alias push_with_commit_message='git add . && git commit -m "$(git show | curl -X POST -d @- https://athletics-hub.uc.r.appspot.com/api/generate-commit-message -H "Content-Type: text/plain")" && git push origin main'
+alias push_with_commit_message='git add . && git commit -m "$(git diff | curl -X POST -d @- https://athletics-hub.uc.r.appspot.com/api/generate-commit-message -H "Content-Type: text/plain")" && git push origin main'
+
+
+
+hello world how are yo udoing 
