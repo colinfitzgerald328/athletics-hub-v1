faa1e832116b3f0574bdb9f29ecfb19e9f2f295e
Colin <colinfitzgerald328@gmail.com>
2024-11-22 22:38:04 -0800

    feat - added better modal

---
+++ app/components/3_Middle/3_CarouselModal/example.tsx
@@ -0,0 +1,113 @@
+import { useAthleteContext } from "../../athlete_context";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "@popmotion/popcorn";
+import "./styles.scss";
+
+enum SwipeDirection {
+  Left = -1,
+  Right = 1,
+}
+
+function Example() {
+  const [[imageCount, direction], setImageCount] = useState([0, 0]);
+  const { athlete } = useAthleteContext();
+
+
+  const activeImageIndex = wrap(0, athlete.athlete.hq_images.length, imageCount);
+
+  const swipeToImage = (swipeDirection: SwipeDirection) => {
+    setImageCount([imageCount + swipeDirection, swipeDirection]);
+  };
+
+  
+  const sliderVariants = {
+    incoming: (direction) => ({
+      x: direction > 0 ? "100%" : "-100%",
+      scale: 1.2,
+      opacity: 0,
+    }),
+    active: { x: 0, scale: 1, opacity: 1 },
+    exit: (direction) => ({
+      x: direction > 0 ? "-100%" : "100%",
+      scale: 1,
+      opacity: 0.2,
+    }),
+  };
+
+  const sliderTransition = {
+    duration: 1,
+    ease: [0.56, 0.03, 0.12, 1.04],
+  };
+
+  const dragEndHandler = (dragInfo) => {
+    const draggedDistance = dragInfo.offset.x;
+    const swipeThreshold = 50;
+    if (draggedDistance > swipeThreshold) {
+      swipeToImage(-1);
+    } else if (draggedDistance < -swipeThreshold) {
+      swipeToImage(1);
+    }
+  };
+
+  const skipToImage = (imageId: number) => {
+    let changeDirection;
+    if (imageId > activeImageIndex) {
+      changeDirection = 1;
+    } else if (imageId < activeImageIndex) {
+      changeDirection = -1;
+    }
+    setImageCount([imageId, changeDirection]);
+  };
+
+  return (
+    <>
+      <div className="slider-container">
+        <div className="slider">
+          <AnimatePresence initial={false} custom={direction}>
+            <motion.div
+              key={imageCount}
+              style={{
+                backgroundImage: `url(${athlete.athlete.hq_images[activeImageIndex]})`,
+              }}
+              custom={direction}
+              variants={sliderVariants}
+              initial="incoming"
+              animate="active"
+              exit="exit"
+              transition={sliderTransition}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(_, dragInfo) => dragEndHandler(dragInfo)}
+              className="image"
+            />
+          </AnimatePresence>
+        </div>
+
+        <div className="buttons">
+          <button onClick={() => swipeToImage(-1)}>PREV</button>
+          <button onClick={() => swipeToImage(1)}>NEXT</button>
+        </div>
+      </div>
+      <div className="thumbnails">
+        {athlete.athlete.hq_images.map((image, index) => (
+          <div
+            key={index}
+            onClick={() => skipToImage(index)}
+            className="thumbnail-container"
+          >
+            <img src={image} alt="" />
+            <div
+              className={`active-indicator ${
+                index === activeImageIndex ? "active" : null
+              }`}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Example;
--- app/components/3_Middle/3_CarouselModal/index.js
+++ app/components/3_Middle/3_CarouselModal/index.js
@@ -1,22 +1,34 @@
-import { Modal, Box } from "@mui/joy";
-import styles from "./styles.module.css";
-import { Example } from "./example";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Example from "./example";
 
-export default function CarouselModal(props) {
-  const style = {
-    display: "flex",
-    alignItems: "center",
-  };
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  width: 800,
+  backgroundColor: "black",
+  borderRadius: "30px",
+};
 
+export default function BasicModal(props) {
   return (
     <Modal
       open={props.carouselModalOpen}
       onClose={() => props.closeCarouselModal()}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
     >
-      <Box onClick={() => props.closeCarouselModal()} sx={style}>
-        <div className={styles.exampleContainer}>
-          <Example />
-        </div>
+      <Box sx={style}>
+        <Example />
       </Box>
     </Modal>
   );
